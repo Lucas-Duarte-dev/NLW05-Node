566aea3a39cff7b71d5fc999e988ba99244cea6a
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-04-19 13:42:08 -0300

    Finalizado o primeiro dia da nlw05 - node

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ hello: "world" });
+});
+
+app.post("/users", (request, response) => {
+  return response.status(200).json({ message: "Usuário criado com sucesso" });
+});
+
+app.listen(3333, () => {
+  console.log("Server is running! 😎");
+});
