33805b6747345038f0b0098f151218e86e811564
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-04-21 11:49:26 -0300

    Criando a migration messages

---
+++ src/database/migration/1619015765108-CreateMessages.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMessages1619015765108 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: "messages",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "admin_id",
+            type: "uuid",
+            isNullable: true,
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "text",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKUser",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("messages");
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+  }
+}
